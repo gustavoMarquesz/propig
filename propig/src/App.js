1d86504101eff35d5517e44cd46ba1cdbc8506df
Gustavo Marques <gustavo99a@hotmail.com>
2023-04-06 10:01:31 -0300

    Arrumando indentações

--- propig/src/App.js
+++ propig/src/App.js
@@ -2,11 +2,13 @@
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import TimeLine from './pages/TimeLine';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <TimeLine/>
       
     </div>
--- propig/src/pages/TimeLine.jsx
+++ propig/src/pages/TimeLine.jsx
@@ -11,29 +11,27 @@ import adressIcons from "../assets/adressIcons.svg"
 function TimeLine(){
     return(
         <section className="tl">
-           <div className="time-line"> 
-            <h1>Acompanhe a trajetória do seu produto</h1>
-            <h1><span>PROPIG</span></h1>
+           <div className="time-line">
             <VerticalTimeline  lineColor="#3e497a">
-                    <VerticalTimelineElement iconStyle={{background: "#0d6efd", color: "white"}} icon={<MapsHomeWork/>}>
-                        <h3>Produto disponível para coleta!</h3>
-                        <p>Nota fiscal emitida.</p>
-                        <p>Agurdando coleta da transportadora</p>
-                    </VerticalTimelineElement>
+                        <VerticalTimelineElement iconStyle={{background: "#0d6efd", color: "white"}} icon={<MapsHomeWork/>}>
+                            <h3>Produto disponível para coleta!</h3>
+                            <p>Nota fiscal emitida.</p>
+                            <p>Agurdando coleta da transportadora</p>
+                        </VerticalTimelineElement>
 
-                    <VerticalTimelineElement iconStyle={{background: "#0d6efd", color: "white"}} icon={<LocalShippingIcon/>}>
-                        <h3>Produto coletado pela transportadora!</h3>
-                        <p>Produto coletado com sucesso.</p>
-                        <p>Em rota para destino</p>
-                    </VerticalTimelineElement>
+                        <VerticalTimelineElement iconStyle={{background: "#0d6efd", color: "white"}} icon={<LocalShippingIcon/>}>
+                            <h3>Produto coletado pela transportadora!</h3>
+                            <p>Produto coletado com sucesso.</p>
+                            <p>Em rota para destino</p>
+                        </VerticalTimelineElement>
 
 
-                    <VerticalTimelineElement iconStyle={{background: "green", color: "white"}} icon={<CheckCircleIcon/>}>
-                        <h4>Produto entregue!</h4>
-                        <h5>recebido: 04/04/2023</h5>
-                        
-                    </VerticalTimelineElement>
-            </VerticalTimeline>
+                        <VerticalTimelineElement iconStyle={{background: "green", color: "white"}} icon={<CheckCircleIcon/>}>
+                            <h4>Produto entregue!</h4>
+                            <h5>recebido: 04/04/2023</h5>
+                            
+                        </VerticalTimelineElement>
+                </VerticalTimeline>
            </div>
 
         </section>
